88107d8ae3c48eb608258553dc18493a88bbce17
Khuslee1 <Khuselee11@gmail.com>
2025-11-07 12:31:18 +0800

    now7day

--- src/_components/Butt.js
+++ src/_components/Butt.js
@@ -3,11 +3,11 @@ import { cn } from "@/_utils/cn";
 export const Butt = (props) => {
   return (
     <div
-      //   className={` bg-[#FF6900] flex justify-center items-center rounded-lg text-white h-9 px-4 ${props.size}`}
       className={cn(
         "bg-[#FF6900] flex justify-center items-center rounded-lg text-white h-9 px-4",
         props.size
       )}
+      onClick={props.state}
     >
       {props.text}
     </div>
--- src/_components/Counter.js
+++ src/_components/Counter.js
@@ -1,33 +1,37 @@
 "use client";
 import { useState } from "react";
+import { Butt } from "@/_components/Butt";
 
 export const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const dec = () => {
-    setCount(count - 1);
-  };
-  const inc = () => {
-    setCount(count + 1);
-  };
-
+  const [input, setInput] = useState([]);
+  const [num, setNum] = useState("");
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="flex gap-4">
-        <button
-          onClick={inc}
-          className="flex justify-center items-center text-black"
-        >
-          +
-        </button>
-        <div className="flex">{count}</div>
-        <button
-          onClick={dec}
-          className="flex justify-center items-center text-black"
-        >
-          -
-        </button>
-      </div>
+    <div className="w-screen h-screen flex justify-center items-center flex-col gap-4">
+      {/* <input
+        className="h-10 w-40 border border-black p-2"
+        placeholder="input"
+        onChange={(e) => setInput(e.target.value)}
+      /> */}
+
+      <input
+        className="h-10 w-40 border border-black p-2"
+        placeholder="number"
+        value={num}
+        onChange={(e) => setNum(e.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          setInput([...input, num]);
+          setNum("");
+        }}
+        className="border border-black rounded-xl text-black h-10 w-40 hover:hover:bg-green-200"
+      >
+        add
+      </button>
+      {input.map((ele) => {
+        return <div key={Math.random()}>{ele}</div>;
+      })}
     </div>
   );
 };
